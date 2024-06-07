53af6d7c34c02d4d3eb541614c7b82a9294e3e1f
mechaneyes <ray@mechaneyes.com>
2024-06-07 10:16:16 -0400

    saving png's ° random colors

--- src/app/class-zine/page.tsx
+++ src/app/class-zine/page.tsx
@@ -1,10 +1,16 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { type Sketch } from "@p5-wrapper/react";
 import { NextReactP5Wrapper } from "@p5-wrapper/next";
+import init, { p5SVG } from "p5.js-svg";
 
-const classZine: Sketch = (p5) => {
+const classZine: Sketch = (p: p5SVG) => {
+  const colorsCols = 7;
+  const colorsRows = 6;
+  const colorsOpacity = 0.6;
+  const size = 124;
+
   // Function to shuffle an array
   const shuffleArray = (array: any[]): any[] => {
     for (let i = array.length - 1; i > 0; i--) {
@@ -14,11 +20,12 @@ const classZine: Sketch = (p5) => {
     return array;
   };
 
-  const colorsCols = 7;
-  const colorsRows = 6;
-  const colorsOpacity = 0.6;
-
-  const size = 124;
+  /* Color Theme Swatches in Hex */
+  // $Miami-Beach #031CA6;
+  // $Miami-Beach #0468BF;
+  // $Miami-Beach #049DD9;
+  // $Miami-Beach #D97904;
+  // $Miami-Beach #D91E1E;
 
   // Create a 2D array to store the colors
   const colors = Array(colorsCols)
@@ -28,53 +35,68 @@ const classZine: Sketch = (p5) => {
   // Generate the colors
   for (let x = 0; x < colorsCols; x++) {
     for (let y = 0; y < colorsRows; y++) {
-      // Cycle through the primary colors
       const color = [
-        Number(x % 3 === 0) * 255,
-        Number(x % 3 === 1) * 255,
-        Number(x % 3 === 2) * 255,
+        Math.floor(Math.random() * 256),
+        Math.floor(Math.random() * 256),
+        Math.floor(Math.random() * 256),
+        0.8,
       ];
       colors[x][y] = color;
     }
   }
 
-  const gridMain = (fillCallback: (p5: any) => void) => {
+  // Get the color array
+  let colorArray;
+
+  for (let x = 0; x < colors.length; x++) {
+    for (let y = 0; y < colors[x].length; y++) {
+      colorArray = colors[x][y];
+    }
+  }
+
+  // Shuffle the color array
+  colorArray = shuffleArray(colorArray);
+
+  // Destructure the color array
+  const [r, g, b] = colorArray;
+
+  const gridMain = (fillCallback: (p: any) => void) => {
     const cols = 8;
     const rows = 5;
 
-    p5.noFill();
-    p5.stroke(0);
-    p5.strokeWeight(2);
+    p.noFill();
+    p.stroke(0);
+    p.strokeWeight(2);
 
     const totalWidth = size * (cols - 1);
     const totalHeight = size * (rows - 1);
 
     for (let x = 0; x < cols; x++) {
       for (let y = 0; y < rows; y++) {
-        p5.push();
-        p5.translate(
-          p5.map(
+        p.push();
+        p.translate(
+          p.map(
             x,
             0,
             cols - 1,
-            p5.width / 2 - totalWidth / 2,
-            p5.width / 2 + totalWidth / 2
+            p.width / 2 - totalWidth / 2,
+            p.width / 2 + totalWidth / 2
           ),
-          p5.map(
+          p.map(
             y,
             0,
             rows - 1,
-            p5.height / 2 - totalHeight / 2,
-            p5.height / 2 + totalHeight / 2
+            p.height / 2 - totalHeight / 2,
+            p.height / 2 + totalHeight / 2
           ),
           0
         );
 
         // Fill is passed as parameter when calling gridMain()
-        fillCallback(p5);
+        fillCallback(p);
 
-        p5.ellipse(0, 0, size, size);
-        p5.pop();
+        p.ellipse(0, 0, size, size);
+        p.pop();
       }
     }
   };
@@ -85,30 +107,30 @@ const classZine: Sketch = (p5) => {
     const cols = 7;
     const rows = 4;
 
-    p5.noFill();
-    p5.stroke(0);
-    p5.strokeWeight(2);
+    p.noFill();
+    p.stroke(0);
+    p.strokeWeight(2);
 
     const totalWidth = size * (cols - 1);
     const totalHeight = size * (rows - 1);
 
     for (let x = 0; x < cols; x++) {
       for (let y = 0; y < rows; y++) {
-        p5.push();
-        p5.translate(
-          p5.map(
+        p.push();
+        p.translate(
+          p.map(
             x,
             0,
             cols - 1,
-            p5.width / 2 - totalWidth / 2,
-            p5.width / 2 + totalWidth / 2
+            p.width / 2 - totalWidth / 2,
+            p.width / 2 + totalWidth / 2
           ),
-          p5.map(
+          p.map(
             y,
             0,
             rows - 1,
-            p5.height / 2 - totalHeight / 2,
-            p5.height / 2 + totalHeight / 2
+            p.height / 2 - totalHeight / 2,
+            p.height / 2 + totalHeight / 2
           ),
           0
         );
@@ -123,48 +145,46 @@ const classZine: Sketch = (p5) => {
         const [r, g, b] = colorArray;
 
         // Set the fill color with 80% opacity
-        p5.fill(r, g, b, 255 * colorsOpacity);
+        p.fill(r, g, b, 255 * colorsOpacity);
 
-        p5.ellipse(0, 0, size, size);
-        p5.pop();
+        p.ellipse(0, 0, size, size);
+        p.pop();
       }
     }
   };
 
-  p5.setup = () => {
-    p5.createCanvas(1024, 664);
-    p5.noLoop();
+  p.setup = () => {
+    const cnv = p.createCanvas(1024, 664, p.SVG);
+    p.noLoop();
+
+    // Scale up the canvas for printing purposes
+    p.pixelDensity(3);
+
+    // Save the canvas as PNG when pressing 's'
+    p.keyPressed = () => {
+      if (p.key === "s") {
+        const date = new Date();
+        const timestamp = date.toISOString().replace(/[:.]/g, "-").slice(0, -5);
+
+        p.save(cnv, `class-zine-${timestamp}.png`);
+      }
+    };
   };
 
-  p5.draw = () => {
-    p5.background(255);
-
-    // Get the color array
-    let colorArray;
-
-    for (let x = 0; x < colors.length; x++) {
-      for (let y = 0; y < colors[x].length; y++) {
-        colorArray = colors[x][y];
-      }
-    }
-
-    // Shuffle the color array
-    colorArray = shuffleArray(colorArray);
-
-    // Destructure the color array
-    const [r, g, b] = colorArray;
+  p.draw = () => {
+    p.background(255);
 
     // Draw 2px stroke around the canvas with aspect ratio 17:11
-    p5.stroke(0);
-    p5.strokeWeight(2);
-    p5.noFill();
-    p5.rect(0, 0, 1024, 664);
+    p.stroke(0);
+    p.strokeWeight(2);
+    p.noFill();
+    p.rect(0, 0, 1024, 664);
 
-    gridMain((p5) => p5.fill(r, g, b, 255 * colorsOpacity));
+    gridMain((p5) => p.fill(r, g, b, 255 * colorsOpacity));
     gridInner();
     gridMain((p5) => {
-      p5.stroke(0); // Set the stroke color to white
-      p5.noFill(); // Disable filling geometry
+      p.stroke(0); // Set the stroke color to white
+      p.noFill(); // Disable filling geometry
     });
   };
 };
